536b1cbb40144f3ce2734e3596e4dcc1d92c26ae
Bruno Henrique Santos <brunohqsantos@hotmail.com>
2021-02-24 14:27:46 -0300

    feat: Get all surveys test

--- src/__tests__/Survey.test.ts
+++ src/__tests__/Survey.test.ts
@@ -17,4 +17,15 @@ describe("Survey", () => {
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty("id");
   });
+
+  it("Should be able to get all surveys", async () => {
+    await request(app).post("/surveys").send({
+      title: "Title Example 2",
+      description: "Description Example 2"
+    });
+
+    const response = await request(app).get("/surveys");
+
+    expect(response.body.length).toBe(2);
+  });
 });
